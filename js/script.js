a833d2d75740af089ff304f39d8708798fdc5186
GuglielmoBini <guglielmobini@gmail.com>
2022-12-21 16:33:05 +0100

    creata struttura html, creato array

--- js/script.js
+++ js/script.js
@@ -23,3 +23,40 @@ Prima la logica in italiano e poi traduciamo in codice.
 */
 
 //-----------------------------------------------------------------------
+
+// prendo gli elementi dal DOM
+const teamCards = document.getElementById("team-cards");
+
+// creo array
+const team = [
+  {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    image: "wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    image: "angela-caroll-chief-editor.jpg",
+  },
+  {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    image: "walter-gordon-office-manager.jpg",
+  },
+  {
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    image: "angela-lopez-social-media-manager.jpg",
+  },
+  {
+    name: "Scott Estrada",
+    role: "Developer",
+    image: "scott-estrada-developer.jpg",
+  },
+  {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    image: "barbara-ramos-graphic-designer.jpg",
+  },
+];
